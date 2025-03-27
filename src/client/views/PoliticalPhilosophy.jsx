fa19d16320fff77a6ab692e74a677cd4d8363c0b
bkhebert <bondman2004tippy@gmail.com>
2025-03-27 16:19:45 -0500

    SETUP route to get political philosophies

--- src/client/views/PoliticalPhilosophy.jsx
+++ src/client/views/PoliticalPhilosophy.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from 'axios';
 
 function PoliticalPhilosophy() {
+
+const getAllTopics = () => {
+  axios.get('api/politicalPhilosophy/getTopics')
+    .then((value) => {
+      console.log('successfully got topics', value);
+    })
+    .catch((err) => {
+      console.error('there is a fuck up w/ topics', error)
+    })
+}
+
+useEffect(() => {
+  getAllTopics();
+})
 
   return (
     <h1 className="p-6 text-center mb-1 text-xl font-semibold text-red-600">ayye</h1>
--- src/server/routes/politicalPhilosophy.js
+++ src/server/routes/politicalPhilosophy.js
@@ -3,4 +3,8 @@ const User = require('../db/models/User');
 
 const politicalPhilosophyRouter = Router();
 
+politicalPhilosophyRouter.get('/getTopics', (req, res) => {
+  res.status(200).send('great job');
+})
+
 module.exports = politicalPhilosophyRouter;
