6f4059c1e7e6263fe8541056b25acfd8b302a351
evan.loria4 <evan.loria4@gmail.com>
2025-03-27 15:44:11 -0500

    Add tailwind div test to Home view

--- src/client/views/Home.jsx
+++ src/client/views/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 function Home() {
   return (
@@ -6,7 +6,12 @@ function Home() {
       <h1>Hello World</h1>
       <div>
         <h4>Log in here!</h4>
-        <button><a href="/api/auth/google">Log in with Google</a></button>
+        <button>
+          <a href="/api/auth/google">Log in with Google</a>
+        </button>
+      </div>
+      <div class="min-h-screen flex items-center justify-center bg-red-primary text-white text-3xl font-bold">
+        Tailwind is working 🎉
       </div>
     </div>
   );
