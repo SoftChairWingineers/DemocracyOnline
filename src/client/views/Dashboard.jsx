9db4424ceff816ea4e095d2e760333dfb1984ce3
evan.loria4 <evan.loria4@gmail.com>
2025-03-27 16:35:20 -0500

    Add top hero section to Dashboard

--- src/client/views/Dashboard.jsx
+++ src/client/views/Dashboard.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React from "react";
+
+const hotTopics = ["Abortion", "Tariffs", "Gun Control"];
 
 function Dashboard() {
   return (
     <div>
-      <h1>Welcome to Democracy Online</h1>
+      <div className="text-center mt-8">
+        <h1 className="text-4xl font-extrabold text-neutral-dark">
+          DemocracyOnline
+        </h1>
+        <p className="text-gray-secondary mt-2 text-lg italic">
+          A platform for balanced political conversations.
+        </p>
+      </div>
+      <div className="w-full h-2 bg-gradient-to-r from-red-primary via-neutral-400 to-blue-primary" />
       <div>
         <h4>If you would like to leave...</h4>
-        <button><a href="/logout">Logout</a></button>
+        <button>
+          <a href="/logout">Logout</a>
+        </button>
       </div>
       <div>
         <h4>Your Current Political Philosophies</h4>
-        <button><a href='/politicalPhilosophy'>Update My Philosophies</a></button>
+        <button>
+          <a href="/politicalPhilosophy">Update My Philosophies</a>
+        </button>
       </div>
     </div>
-
   );
 }
 
