47de5b07fe6583038cc856baeba5327e91712000
evan.loria4 <evan.loria4@gmail.com>
2025-03-27 18:16:30 -0500

    Initialize NewsBox component

---
+++ src/client/components/NewsBox.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NewsBox() {
+  return (
+    <div className="flex flex-col gap-2">
+    <div className="text-xl">Current News</div>
+    <div className="border border-2 min-h-[300px]"></div>
+    </div>
+  )
+}
