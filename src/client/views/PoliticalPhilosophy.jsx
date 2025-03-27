19148464860055ec31f561c49cf9af3829a7bbcc
bkhebert <bondman2004tippy@gmail.com>
2025-03-27 16:11:12 -0500

    ADD reference to political philosophy table

--- src/client/views/PoliticalPhilosophy.jsx
+++ src/client/views/PoliticalPhilosophy.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function PoliticalPhilosophy() {
 
   return (
-    <h1 className="p-6 text-center mb-1 text-xl font-semibold text-red-100">ayye</h1>
+    <h1 className="p-6 text-center mb-1 text-xl font-semibold text-red-600">ayye</h1>
   )
 
 }
