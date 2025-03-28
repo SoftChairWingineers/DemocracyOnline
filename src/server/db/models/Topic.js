7b7331ae1ec46653235e3ef9ef84a606f342e77f
tymey <tyler.meyer.a@gmail.com>
2025-03-27 20:57:28 -0500

    build: Add Topics table to database

---
+++ src/server/db/models/Topic.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('@sequelize/core');
+const database = require('../index');
+
+const Topic = database.define(
+  'Topic',
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {
+    tableName: 'Topics', // Explicitly specify table name
+    timestamps: true, // Enables createdAt & updatedAt fields
+  },
+);
+
+module.exports = Topic;
