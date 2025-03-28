254cede4907ca46c2d6bdde0273fc558f517efd0
tymey <tyler.meyer.a@gmail.com>
2025-03-28 01:13:07 -0500

    feat: Finish POST /api/message handler

--- src/server/routes/message.js
+++ src/server/routes/message.js
@@ -1,16 +1,34 @@
 const { Router } = require('express');
 
+const Message = require('../db/models/Message');
+
 const messageRouter = Router();
 
 /*
   POST /api/message
-    - Send fact checked and neutralized message in request body with topic ID: { post: { message, topicId, replyId? } }
+    - Send fact checked and neutralized message in request body with topic ID: { message: { content, topicId, respondingTo? } }
     - Store message in database with topic ID and user ID
       - If the message is replying to a specific message, include the id of the message being replied to
     - Send status 201 for successful POST
 */
-messageRouter.post('/', (req, res) => {
-  res.sendStatus(201);
+messageRouter.post('/', async (req, res) => {
+  if (!req.body.message) { // Check for message object on req.body
+    res.sendStatus(400);
+  } else {
+    const { message } = req.body;
+    if (!message.content || !message.topicId) { // Check for content & topicId on message object
+      res.sendStatus(400);
+    } else {
+      message.userId = req.user.id; // Add userId to message object using req.user.id
+      try {
+        await Message.create(message); // Insert message into Messages table
+        res.sendStatus(201);
+      } catch (error) {
+        console.error('Failed to POST /api/message');
+        res.sendStatus(500);
+      }
+    }
+  }
 });
 
 /*
