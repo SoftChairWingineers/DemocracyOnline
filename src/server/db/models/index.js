0a10900336ad50bcc40d58b251d87a8d28631379
tymey <tyler.meyer.a@gmail.com>
2025-03-27 21:45:05 -0500

    build: Create index file for all models

---
+++ src/server/db/models/index.js
@@ -0,0 +1,13 @@
+const Message = require('./Message');
+const PoliticalView = require('./PoliticalViews');
+const SuggestedTopic = require('./SuggestedTopic');
+const Topic = require('./Topic');
+const User = require('./User');
+
+module.exports = {
+  Message,
+  PoliticalView,
+  SuggestedTopic,
+  Topic,
+  User,
+};
