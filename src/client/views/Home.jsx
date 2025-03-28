a67f8d3614fd68d811951b07defb7b046a04fcb9
evan.loria4 <evan.loria4@gmail.com>
2025-03-28 16:45:03 -0500

    More styling to Home landing page

--- src/client/views/Home.jsx
+++ src/client/views/Home.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="flex flex-col items-center justify-center bg-neutral-light px-4">
+    <div className="flex flex-col items-center mt-24 gap-4">
       <p className="text-neutral-dark text-center text-xl max-w-xl mb-8 leading-relaxed px-4">
         A platform for open political debate, powered by real-time
         fact-checking. Share your opinions and let the facts speak.
       </p>
       <button>
-        <a className="font-semibold" href="/api/auth/google">
+        <a className="font-semibold px-4 py-2 bg-blue-primary text-neutral-light rounded-lg" href="/api/auth/google">
           Log in with Google
         </a>
       </button>
