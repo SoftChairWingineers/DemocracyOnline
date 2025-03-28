c266a3cd938aac0e2f5483157a0fdf038f39d7ae
tymey <tyler.meyer.a@gmail.com>
2025-03-27 21:42:16 -0500

    move: Database sync happens in server file

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,12 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-require('./db');
+const database = require('./db');
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  await database.sync({ force: true });
   console.log(`Server listening at http://localhost:${PORT}`);
 });
