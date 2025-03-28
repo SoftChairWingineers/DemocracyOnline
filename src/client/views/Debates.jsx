6def90c771b16c814615deab45f6d7981963f6ff
tymey <tyler.meyer.a@gmail.com>
2025-03-28 14:05:29 -0500

    feat: Incorporate MessageList comp into Debates view

--- src/client/views/Debates.jsx
+++ src/client/views/Debates.jsx
@@ -3,6 +3,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 import PostMessage from '../components/debates/PostMessage';
+import MessageList from '../components/debates/MessageList';
 
 function Debates(){
   const location = useLocation();
@@ -48,33 +49,7 @@ function Debates(){
         Return to Dashboard
       </button>
       <PostMessage getMessages={getMessages} topicId={topic.id} />
-      <div>
-        {messages.map((msg) => (
-          <div key={msg.id} className="mb-4 p-3 border rounded">
-            <p>{msg.content}</p>
-            {/* <div className="mt-2 ml-4">
-              {msg.replies.map((reply) => (
-                <p key={reply.id} className="text-sm text-gray-700">↳ {reply.text}</p>
-              ))}
-            </div>
-            <div className="mt-2">
-              <input
-                type="text"
-                className="w-full p-1 border rounded"
-                placeholder="Reply..."
-                value={replies[msg.id] || ""}
-                onChange={(e) => setReplies({ ...replies, [msg.id]: e.target.value })}
-              />
-              <button
-                className="mt-1 bg-red-primary text-white px-2 py-1 rounded"
-                onClick={() => handleReply(msg.id, replies[msg.id] || "")}
-              >
-                Reply
-              </button>
-            </div> */}
-          </div>
-        ))}
-      </div>
+      <MessageList getMessages={getMessages} messages={messages} />
     </div>
     </div>
   );
