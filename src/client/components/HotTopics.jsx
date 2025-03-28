06dc37ab33e7f12c2e90abcf40a55a01ba749e82
tymey <tyler.meyer.a@gmail.com>
2025-03-27 22:38:51 -0500

    feat: Fetch DB topics on HotTopics comp mount

--- src/client/components/HotTopics.jsx
+++ src/client/components/HotTopics.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState, useCallback, useEffect } from "react";
+import axios from 'axios';
 
 const hotTopics = ["Abortion", "Tariffs", "Gun Control", "The Border", "DOGE", "Middle East"];
 
 export default function HotTopics() {
+  const [topics, setTopics] = useState([]);
+
+  const getTopics = useCallback(() => {
+    axios.get('/api/topic')
+    .then(({ data }) => {
+      setTopics(data);
+    })
+    .catch((error) => {
+      console.error('Failed to getTopics:', error);
+    });
+  }, []);
+
+  useEffect(() => {
+    getTopics();
+  }, [getTopics]);
+
+  console.log('Topics: ', topics);
+
   return (
     <div className="flex flex-row flex-wrap gap-4 py-4 max-w-[75%] mx-auto justify-center">
       {hotTopics.map((topic) => (
