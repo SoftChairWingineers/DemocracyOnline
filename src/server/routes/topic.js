a8f16cb38ff644aaae9005fdac1b54e1b0e5e682
tymey <tyler.meyer.a@gmail.com>
2025-03-27 22:31:53 -0500

    feat: GET /api/topic returns topics in DB, alphabet order

--- src/server/routes/topic.js
+++ src/server/routes/topic.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 
+const Topic = require('../db/models/Topic');
+
 const topicRouter = Router();
 
 /*
@@ -8,8 +10,19 @@ const topicRouter = Router();
     - Format: Array
     - Status: 200
 */
-topicRouter.get('/', (req, res) => {
-  res.sendStatus(200);
+topicRouter.get('/', async (req, res) => {
+  try {
+    const topics = await Topic.findAll({
+      order: [
+        ['name', 'ASC'],
+      ],
+    });
+    res.status(200).send(topics);
+  } catch (error) {
+    console.error('Failed to GET /api/topic ', error);
+    res.sendStatus(500);
+  }
+  
 });
 
 module.exports = topicRouter;
