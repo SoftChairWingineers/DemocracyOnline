96a4ff0469439fd4170947592c5428b3a918b892
evan.loria4 <evan.loria4@gmail.com>
2025-03-27 22:48:05 -0500

    Create articles router to make req to GNews API

--- src/server/routes/articles.js
+++ src/server/routes/articles.js
@@ -1,1 +1,23 @@
-import axios from 'axios';
+const { Router } = require('express');
+const dotenv = require('dotenv');
+const axios = require('axios');
+
+dotenv.config();
+
+const { GNews_API } = process.env;
+
+const articlesRouter = Router();
+
+articlesRouter.get('/articles', (res, req) => {
+  // Make request to GNews API
+  axios.get(`https://gnews.io/api/v4/top-headlines?lang=en&country=us&max=10&apikey=${GNews_API}`)
+  .then((articles) => {
+    console.log(articles);
+    res.statusCode(200).send(articles);
+  })
+  .catch((err) => {
+    console.error('Error with request to GNews in articles router: ', err);
+  });
+})
+
+module.exports = articlesRouter;
