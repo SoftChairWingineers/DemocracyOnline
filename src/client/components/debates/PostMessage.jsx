7710635b1632fe3f73f23be2cd45eef3d9860393
tymey <tyler.meyer.a@gmail.com>
2025-03-28 13:57:16 -0500

    feat: Before message gets published, AI checks

--- src/client/components/debates/PostMessage.jsx
+++ src/client/components/debates/PostMessage.jsx
@@ -1,22 +1,27 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Loader } from 'lucide-react';
 
 function PostMessage({ getMessages, topicId }) {
-  const [newMessage, setNewMessage] = useState("");
+  const [newMessage, setNewMessage] = useState('');
+  const [aiResponse, setAiResponse] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   
   const postMessage = () => {
-    if (newMessage.trim() === "") {
+    if (aiResponse === null) {
       return;
     }
     axios.post('/api/message', {
       message: {
-        content: newMessage,
+        content: aiResponse.factCheckedMessage,
         topicId,
       },
     })
       .then(getMessages)
       .then(() => {
         setNewMessage('');
+        setAiResponse(null);
+        setIsLoading(true);
       })
       .catch((error) => {
         console.error('Failed to postMessage: ', error);
@@ -24,10 +29,40 @@ function PostMessage({ getMessages, topicId }) {
   };
 
   const aiFactChecker = () => {
-    postMessage();
+    axios.post('/api/ai/fact', {
+      message: newMessage,
+    })
+      .then(({ data }) => {
+        setAiResponse(data);
+      })
+      .catch((error) => {
+        console.error('Failed to fact check with AI: ', error);
+      });
   }
 
   return (
+    aiResponse ? (
+      <div className="mb-4">
+        <h1 className="text-xl font-bold mb-4">Updated Message to Post</h1>
+        <p className="mb-4">{aiResponse.factCheckedMessage}</p>
+        <h1 className="text-xl font-bold mb-4">Reason for Change</h1>
+        <p className="mb-4">{aiResponse.factCheckedStatement}</p>
+        <div>
+          <button
+            className="mt-2 bg-blue-primary text-white px-4 py-2 rounded mr-2"
+            onClick={() => { setAiResponse(null); }}
+          >
+            Back to the Drawing Board
+          </button>
+          <button
+            className="mt-2 bg-blue-primary text-white px-4 py-2 rounded"
+            onClick={postMessage}
+          >
+            Submit Updated Message
+          </button>
+        </div>
+      </div>
+    ) : (
     <div className="mb-4">
       <textarea
         className="w-full p-2 border rounded"
@@ -42,6 +77,7 @@ function PostMessage({ getMessages, topicId }) {
         Post Message
       </button>
     </div>
+    )
   );
 }
 
