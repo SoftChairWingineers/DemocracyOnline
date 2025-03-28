b8af48e78aa4c754a4fc0232c1959b5c94d7df73
evan.loria4 <evan.loria4@gmail.com>
2025-03-28 16:34:07 -0500

    Update home with more styling

--- src/client/views/Home.jsx
+++ src/client/views/Home.jsx
@@ -2,13 +2,16 @@ import React from "react";
 
 function Home() {
   return (
-    <div>
-      <div>
-        <h4>Log in here!</h4>
-        <button>
-          <a href="/api/auth/google">Log in with Google</a>
-        </button>
-      </div>
+    <div className="flex flex-col items-center justify-center bg-neutral-light px-4">
+      <p className="text-neutral-dark text-center text-xl max-w-xl mb-8 leading-relaxed px-4">
+        A platform for open political debate, powered by real-time
+        fact-checking. Share your opinions and let the facts speak.
+      </p>
+      <button>
+        <a className="font-semibold" href="/api/auth/google">
+          Log in with Google
+        </a>
+      </button>
     </div>
   );
 }
