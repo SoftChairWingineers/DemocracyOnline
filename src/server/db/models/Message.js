fdde4d16c741343fce060e9f9cb19ba2279d0c6f
tymey <tyler.meyer.a@gmail.com>
2025-03-27 21:58:45 -0500

    fix: Message belongs to another message, origin

--- src/server/db/models/Message.js
+++ src/server/db/models/Message.js
@@ -14,14 +14,9 @@ const Message = database.define(
     respondingTo: {
       type: DataTypes.INTEGER,
       allowNull: true,
-      references: {
-        model: 'Messages',
-        key: 'id'
-      }
     }
   },
   {
-    tableName: 'Messages', // Explicitly specify table name
     timestamps: true, // Enables createdAt & updatedAt fields
   }
 );
@@ -29,7 +24,6 @@ const Message = database.define(
 /*
   Associations for Messages:
 */
-Message.hasMany(Message, { as: 'responses', foreignKey: 'respondingTo' });
 Message.belongsTo(Message, { as: 'origin', foreignKey: 'respondingTo' });
 
 Message.belongsTo(User); // UserId
