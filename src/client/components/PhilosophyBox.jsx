7747ddbb56d627701b6e30bd1cd12171fedfb993
evan.loria4 <evan.loria4@gmail.com>
2025-03-29 09:17:37 -0500

    Make PhilosophyBox click navigate to view

--- src/client/components/PhilosophyBox.jsx
+++ src/client/components/PhilosophyBox.jsx
@@ -121,7 +121,7 @@ const processResponses = (responses) => {
 
   return (
 <div
-  
+  onClick={() => window.location.href = "/politicalPhilosophy"}
   className="border-blue-primary border-2 rounded-3xl p-4 w-full md:max-w-[600px] 
     lg:w-1/5 min-h-[150px] md:min-h-[150px] lg:min-h-[350px] hover:cursor-pointer 
     sm:mb-6 md:mb-6 hover:bg-gray-300/30 hover:shadow-md hover:scale-105 transition flex flex-col items-center"
